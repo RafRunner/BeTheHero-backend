28007b3c268334df302384519cc56a10af4b453b
Rafael Nunes <rafaelns.br@gmail.com>
2020-03-29 22:31:39 -0300

    Adicionando validação de parâmetros nas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const express = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
+
 const OngController = require("./controllers/OngController");
 const CasoController = require("./controllers/CasoController");
 const ProfileController = require("./controllers/ProfileController");
@@ -9,14 +11,80 @@ const SessionController = require("./controllers/SessionController");
 const routes = express.Router();
 
 routes.get("/ongs", OngController.index);
-routes.post("/ongs", OngController.create);
+routes.post(
+  "/ongs",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      nome: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.number()
+        .required()
+        .min(10)
+        .max(11),
+      cidade: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2)
+    })
+  }),
+  OngController.create
+);
 
-routes.get("/casos", CasoController.index);
-routes.post("/casos", CasoController.create);
-routes.delete("/casos/:id", CasoController.delete);
+routes.get(
+  "/casos",
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number().min(1),
+      page_size: Joi.number().min(1)
+    })
+  }),
+  CasoController.index
+);
 
-routes.get("/profile", ProfileController.index);
+routes.post(
+  "/casos",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      titulo: Joi.string().required(),
+      descricao: Joi.string().required(),
+      valor: Joi.number()
+        .required()
+        .min(1)
+    })
+  }),
+  CasoController.create
+);
 
-routes.post("/sessions", SessionController.create);
+routes.delete(
+  "/casos/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+    })
+  }),
+  CasoController.delete
+);
+
+routes.get(
+  "/profile",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+  ProfileController.index
+);
+
+routes.post(
+  "/sessions",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      id: Joi.string().required()
+    })
+  }),
+  SessionController.create
+);
 
 module.exports = routes;
